c206150233ab855a985fa5f7179b304dfd784a1c
Patricia Coelho <patyleto13@gmail.com>
2020-03-15 18:03:19 -0300

    inserted function filter and lambda in the code

--- compiladoresTrab01.py
+++ compiladoresTrab01.py
@@ -15,24 +15,43 @@ Opcional: Elimine os comentários definidos na sua linguagem
 
 import re 
 #delititado por espaço
-exemplo = '42 + (675 * 31) -20925 + a'
+exemplo = '42 + ( 675 * 31 ) -20925 + a ** B c D'
+
 table_of_operations = {'*':'Mutiplicacao', '**':'Potenciacao',
 '+':'soma', '-':'subtracao', '/': 'divisao'}
 
-numeros=[]
+#
+string=[]
+numero=[]
+operations=[]
+a=[]
 teste =re.split(r' ', exemplo)
+
+#aqui estamos usando um regex para encontrar todos os numero (\d)
+#e inserir eles na em x1, usando uma função lambda e a filter (abaixo fazemos um cast de filter para list)
+x1= filter(lambda i: re.search(r'(?i)\d',i,re.I), teste)
+numeros= list(x1)
+#usando expressao regular para encontrar o (
+x2 =filter(lambda i:re.search(r'\(', i),teste)
+x_2=list(x2)
+
+#usando expressao regular para encontrar o (
+x3 =filter(lambda i:re.search(r'\)', i),teste)
+x_3=list(x3)
+
+x4= filter(lambda i:re.findall(r'[A-Z-a-b]', i),teste)
+x_4=list(x4)
+ #operacoes       
 for i in teste:
-    if re.search(r'(?i)\d',i,re.I): #um jeito de nao pegar os parenteses
-        numeros.append(i)
+    if re.findall(r'[A-Z-a-b]', i):#problema ta no regex
+        string.append(i) #ele ta pegando o valor negativo -20925
+
+#DEU PROBLEMA AQUI        
+#x5=filter(lambda i:re.findall(r'[+ / * - ** =]',i),exemplo)
+#x_5=list(x5)
+
+#AQUI FUNCIONOU DIREITO
 for i in teste:
-    if re.search(r'\(', i):
-        numeros.append('PARETESQ') #mudar por uma funçaõ lambda
-for i in teste:
-    if re.search(r'\)', i):
-        numeros.append('PAREDIR') #mudar por uma funçaõ lambda
-        
-for i in teste:
-    if re.findall(r'[a-zA]', i):
-        numeros.append('Variavel')
-for i in teste:
-    if re.findall(r'+ - ')
+    if re.findall(r'[+ / * - ** =]',i):
+        operations.append(i)
+   
